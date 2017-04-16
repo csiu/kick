cfb8ea4a3c949d5a8cc5340b365d6db353018ee4
csiu <celia.sy.siu@gmail.com>
2017-04-15 23:06:32 -0700

    if none, return "" + rm trailing whitespace

--- src/python/preprocess.py
+++ src/python/preprocess.py
@@ -8,27 +8,29 @@ class DatabaseKick:
     def __init__(self):
         self.dbname = "kick"
         self.tblname = "info"
-    
+
     def connect(self):
         self.conn = psycopg2.connect(dbname=self.dbname)
         self.cur = self.conn.cursor()
         return(self.cur)
-    
+
     def disconnect(self):
         self.cur.close()
         self.conn.close()
 
 def text_processing(text, method=None):
+    if text == None: return("")
+
     # Lower case
     text = text.lower()
-    
+
     # Remove non-letters &
-    # Tokenize    
+    # Tokenize
     words = nltk.wordpunct_tokenize(re.sub('[^a-zA-Z_ ]', '', text))
-    
+
     # Remove stop words
     words = [w for w in words if not w in stopwords.words("english")]
-    
+
     # Stemming vs Lemmatizing vs do nothing
     if method == "stem":
         port = PorterStemmer()
